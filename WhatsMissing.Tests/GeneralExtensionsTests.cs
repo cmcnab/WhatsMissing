129fd57f730c008036ce8620cfe189401f832b51
Chadwick McNab <cmcnab@gmail.com>
2013-08-05 13:16:48 -0500

    Added a general maybe monad in the form of an IfNotNull extension method.

---
+++ WhatsMissing.Tests/GeneralExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿namespace WhatsMissing.Tests
+{
+    using System;
+    using Xunit;
+
+    public class GeneralExtensionsTests
+    {
+        [Fact]
+        public void ToStringNotNullObjectReturnsString()
+        {
+            // Arrange
+            Uri uri = new Uri("http://localhost");
+
+            // Act
+            var str = uri.IfNotNull(u => u.ToString());
+
+            // Assert
+            Assert.Equal(uri.ToString(), str);
+        }
+
+        [Fact]
+        public void ToStringNullObjectReturnsNull()
+        {
+            // Arrange
+            Uri uri = null;
+
+            // Act
+            var str = uri.IfNotNull(u => u.ToString());
+
+            // Assert
+            Assert.Null(str);
+        }
+
+        [Fact]
+        public void ChainedIfNotNull_FirstNull_ExpressionIsDefault()
+        {
+            // Arrange
+            Uri uri = null;
+
+            // Act
+            var len = uri.IfNotNull(u => u.ToString()).IfNotNull(s => s.Length);
+
+            // Assert
+            Assert.Equal(default(int), len);
+        }
+    }
+}
---
+++ WhatsMissing/GeneralExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace WhatsMissing
+{
+    using System;
+
+    public static class GeneralExtensions
+    {
+        public static R IfNotNull<T, R>(this T obj, Func<T, R> fn) where T : class
+        {
+            return obj == null ? default(R) : fn(obj);
+        }
+    }
+}
