abfc7af6936bfdf63e584524ec2b514b0b83345c
Chadwick McNab <cmcnab@gmail.com>
2013-08-11 20:04:10 -0500

    Added a params helper to create a new list.

--- WhatsMissing/Params.cs
+++ WhatsMissing/Params.cs
@@ -1,10 +1,17 @@
 ﻿namespace WhatsMissing
 {
+    using System.Collections.Generic;
+
     public static class Params
     {
         public static T[] Get<T>(params T[] args)
         {
             return args;
+        }
+
+        public static List<T> List<T>(params T[] args)
+        {
+            return new List<T>(args);
         }
     }
 }
