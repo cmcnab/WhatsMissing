b1573a9b89ff1e49a2b714945f69e99daf404cd7
Chadwick McNab <cmcnab@gmail.com>
2013-10-02 11:17:33 -0500

    Adding a hashcode helper class.

---
+++ WhatsMissing.Tests/HashCodeTests.cs
@@ -0,0 +1,87 @@
+﻿namespace WhatsMissing.Tests
+{
+    using Xunit;
+
+    public class HashCodeTests
+    {
+        [Fact]
+        public void HashCode_2ObjectsWithNulls_AreEqual()
+        {
+            // Arrange
+            var obj1 = new AClass(null, 0);
+            var obj2 = new AClass(null, 0);
+
+            // Act
+            var hash1 = obj1.GetHashCode();
+            var hash2 = obj2.GetHashCode();
+            
+            // Assert
+            Assert.Equal(hash1, hash2);
+        }
+
+        [Fact]
+        public void HashCode_1ObjectWith1Not_AreNotEqual()
+        {
+            // Arrange
+            var obj1 = new AClass(null, 0);
+            var obj2 = new AClass("foo", 3);
+
+            // Act
+            var hash1 = obj1.GetHashCode();
+            var hash2 = obj2.GetHashCode();
+
+            // Assert
+            Assert.NotEqual(hash1, hash2);
+        }
+
+        [Fact]
+        public void HashCode_ObjectsDiffOrder_AreNotEqual()
+        {
+            // Arrange
+            var obj1 = new AClass("foo", 3);
+            var obj2 = new BClass(3, "foo");
+
+            // Act
+            var hash1 = obj1.GetHashCode();
+            var hash2 = obj2.GetHashCode();
+
+            // Assert
+            Assert.NotEqual(hash1, hash2);
+        }
+
+        private class AClass
+        {
+            private string s;
+            private int i;
+
+            public AClass(string s, int i)
+            {
+                this.s = s;
+                this.i = i;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.ComposeNotNull(this.s).And(this.i);
+            }
+        }
+
+        private class BClass
+        {
+            private int i;
+            private string s;
+            
+
+            public BClass(int i, string s)
+            {
+                this.i = i;
+                this.s = s;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Compose(this.i).AndNotNull(this.s);
+            }
+        }
+    }
+}
---
+++ WhatsMissing/HashCode.cs
@@ -0,0 +1,49 @@
+﻿namespace WhatsMissing
+{
+    public struct HashCode
+    {
+        private const int Factor = 31;
+        private readonly int hashCode;
+
+        public HashCode(int hashCode)
+        {
+            this.hashCode = hashCode;
+        }
+
+        public static implicit operator int(HashCode hashCode)
+        {
+            return hashCode.GetHashCode();
+        }
+
+        public static HashCode Compose<T>(T arg) where T : struct
+        {
+            return new HashCode(arg.GetHashCode());
+        }
+
+        public static HashCode ComposeNotNull<T>(T arg) where T : class
+        {
+            return new HashCode(arg == null ? Factor : arg.GetHashCode());
+        }
+
+        public HashCode And<T>(T arg) where T : struct
+        {
+            unchecked
+            {
+                return new HashCode((this.hashCode * Factor) + arg.GetHashCode());
+            }
+        }
+
+        public HashCode AndNotNull<T>(T arg) where T : class
+        {
+            unchecked
+            {
+                return new HashCode((this.hashCode * Factor) + (arg == null ? 0 : arg.GetHashCode()));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.hashCode;
+        }
+    }
+}
