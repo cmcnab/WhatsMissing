40df33947fc7bdb45c65e5b16551e6e8dc0aabb4
Chadwick McNab <cmcnab@gmail.com>
2013-12-04 07:18:28 -0600

    Added some common string conversion helpers.

---
+++ WhatsMissing.Tests/StringConversionExtensionTests.cs
@@ -0,0 +1,162 @@
+﻿namespace WhatsMissing.Tests
+{
+    using Xunit;
+
+    public class StringConversionExtensionTests
+    {
+        #region EmptyIfNull
+
+        [Fact]
+        public void EmptyIfNull_Null_ReturnsEmpty()
+        {
+            // Arrange
+            string input = null;
+
+            // Act
+            var output = input.EmptyIfNull();
+
+            // Assert
+            Assert.Equal(string.Empty, output);
+        }
+
+        [Fact]
+        public void EmptyIfNull_NotNull_ReturnsValue()
+        {
+            // Arrange
+            string input = "123";
+
+            // Act
+            var output = input.EmptyIfNull();
+
+            // Assert
+            Assert.Equal(input, output);
+        }
+
+        #endregion
+
+        #region AsBool
+
+        [Fact]
+        public void AsBool_Null_ReturnsNull()
+        {
+            // Arrange
+            string input = null;
+
+            // Act
+            var output = input.AsBool();
+
+            // Assert
+            Assert.True(output == null);
+        }
+
+        [Fact]
+        public void AsBool_NotABool_ReturnsNull()
+        {
+            // Arrange
+            string input = "fooey";
+
+            // Act
+            var output = input.AsBool();
+
+            // Assert
+            Assert.True(output == null);
+        }
+
+        [Fact]
+        public void AsBool_IsABool_ReturnsValue()
+        {
+            // Arrange
+            string input = "true";
+
+            // Act
+            var output = input.AsBool();
+
+            // Assert
+            Assert.Equal(true, output.Value);
+        }
+
+        [Fact]
+        public void AsBool_IsABoolMixedCase_ReturnsValue()
+        {
+            // Arrange
+            string input = "True";
+
+            // Act
+            var output = input.AsBool();
+
+            // Assert
+            Assert.Equal(true, output.Value);
+        }
+
+        #endregion
+
+        #region AsBoolOrDefault
+
+        [Fact]
+        public void AsBoolOrDefault_Null_ReturnsDefault()
+        {
+            // Arrange
+            string input = null;
+
+            // Act
+            var output = input.AsBoolOrDefault();
+
+            // Assert
+            Assert.Equal(default(bool), output);
+        }
+
+        [Fact]
+        public void AsBoolOrDefault_NotABool_ReturnsDefault()
+        {
+            // Arrange
+            string input = "fooey";
+
+            // Act
+            var output = input.AsBoolOrDefault();
+
+            // Assert
+            Assert.Equal(default(bool), output);
+        }
+
+        [Fact]
+        public void AsBoolOrDefault_NotABoolWithOverride_ReturnsOverride()
+        {
+            // Arrange
+            string input = "fooey";
+
+            // Act
+            var output = input.AsBoolOrDefault(true);
+
+            // Assert
+            Assert.Equal(true, output);
+        }
+
+        [Fact]
+        public void AsBoolOrDefault_IsABool_ReturnsValue()
+        {
+            // Arrange
+            string input = "true";
+
+            // Act
+            var output = input.AsBoolOrDefault();
+
+            // Assert
+            Assert.Equal(true, output);
+        }
+
+        [Fact]
+        public void AsBoolOrDefault_IsABoolMixedCase_ReturnsValue()
+        {
+            // Arrange
+            string input = "True";
+
+            // Act
+            var output = input.AsBoolOrDefault();
+
+            // Assert
+            Assert.Equal(true, output);
+        }
+
+        #endregion
+    }
+}
---
+++ WhatsMissing/StringConversionExtensions.cs
@@ -0,0 +1,92 @@
+﻿namespace WhatsMissing
+{
+    public static class StringConversionExtensions
+    {
+        #region String
+
+        public static string EmptyIfNull(this string s)
+        {
+            return s == null ? string.Empty : s;
+        }
+
+        #endregion
+
+        #region Bool
+
+        public static bool? AsBool(this string s)
+        {
+            bool result;
+            if (!string.IsNullOrEmpty(s) && bool.TryParse(s, out result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+
+        public static bool AsBoolOrDefault(this string s)
+        {
+            return AsBoolOrDefault(s, default(bool));
+        }
+
+        public static bool AsBoolOrDefault(this string s, bool defaultValue)
+        {
+            var result = AsBool(s);
+            return result.HasValue ? result.Value : defaultValue;
+        }
+
+        #endregion
+
+        #region Int
+
+        public static int? AsInt(this string s)
+        {
+            int result;
+            if (!string.IsNullOrEmpty(s) && int.TryParse(s, out result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+
+        public static int AsIntOrDefault(this string s)
+        {
+            return AsIntOrDefault(s, default(int));
+        }
+
+        public static int AsIntOrDefault(this string s, int defaultValue)
+        {
+            var result = AsInt(s);
+            return result.HasValue ? result.Value : defaultValue;
+        }
+
+        #endregion
+
+        #region Double
+
+        public static double? AsDouble(this string s)
+        {
+            int result;
+            if (!string.IsNullOrEmpty(s) && int.TryParse(s, out result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+
+        public static double AsDoubleOrDefault(this string s)
+        {
+            return AsDoubleOrDefault(s, default(double));
+        }
+
+        public static double AsDoubleOrDefault(this string s, double defaultValue)
+        {
+            var result = AsDouble(s);
+            return result.HasValue ? result.Value : defaultValue;
+        }
+
+        #endregion
+    }
+}
