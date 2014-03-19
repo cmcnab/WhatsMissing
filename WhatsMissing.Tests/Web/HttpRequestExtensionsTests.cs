11a166acf966b64672e936d41bca17d8478a3217
Chadwick McNab <cmcnab@gmail.com>
2014-03-19 14:36:07 -0500

    Adding some more tests.

--- WhatsMissing.Tests/Web/HttpRequestExtensionsTests.cs
+++ WhatsMissing.Tests/Web/HttpRequestExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace WhatsMissing.Tests.Web
 {
+    using System.IO;
     using System.Threading.Tasks;
     using Moq;
     using WhatsMissing.Web;
@@ -46,6 +47,64 @@
 
             // Act
             var result = request.Object.HttpGetAsync(s => int.Parse(s.ReadToEnd())).Result;
+
+            // Assert
+            Assert.Equal(7856, result);
+        }
+
+        [Fact]
+        public void HttpPostAsync_SetsMethodToPost()
+        {
+            // Arrange
+            var request = new Mock<IHttpRequest>();
+
+            // Act
+            request.Object.HttpPostAsync().Wait();
+
+            // Assert
+            request.VerifySet(r => r.Method = "POST", Times.Once());
+        }
+
+        [Fact]
+        public void HttpPostAsync_WithContent_SetsContentType()
+        {
+            // Arrange
+            var request = new Mock<IHttpRequest>();
+
+            // Act
+            request.Object.HttpPostAsync("application/json", s => { }).Wait();
+
+            // Assert
+            request.VerifySet(r => r.ContentType = "application/json", Times.Once());
+        }
+
+        [Fact]
+        public void HttpPostAsync_WithContent_RequestStreamIsPassed()
+        {
+            // Arrange
+            var request = new Mock<IHttpRequest>();
+            var requestStream = new MemoryStream();
+            request.Setup(r => r.GetRequestStreamAsync()).Returns(Task.FromResult<Stream>(requestStream));
+
+            // Act
+            Stream capturedStream = null;
+            request.Object.HttpPostAsync("application/json", s => capturedStream = s).Wait();
+
+            // Assert
+            Assert.Equal(requestStream, capturedStream);
+        }
+
+        [Fact]
+        public void HttpPostAsync_ResponseHasContent_ContentDeserialized()
+        {
+            // Arrange
+            var request = new Mock<IHttpRequest>();
+            var response = new Mock<IHttpResponse>();
+            response.Setup(r => r.GetResponseStream()).Returns(MemoryStreams.CreateWith("7856"));
+            request.Setup(r => r.GetResponseAsync()).Returns(Task.FromResult(response.Object));
+
+            // Act
+            var result = request.Object.HttpPostAsync("application/json", null, s => int.Parse(s.ReadToEnd())).Result;
 
             // Assert
             Assert.Equal(7856, result);
