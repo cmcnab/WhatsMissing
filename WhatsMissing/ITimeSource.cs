c16f8a6217962b1ca237b13dc218d8fee12c54bb
Chadwick McNab <cmcnab@gmail.com>
2014-09-01 13:14:28 -0400

    Added DateTimeOffset accessors to ITimeSource.

--- WhatsMissing/ITimeSource.cs
+++ WhatsMissing/ITimeSource.cs
@@ -5,5 +5,18 @@
     public interface ITimeSource
     {
         DateTime UtcNow { get; }
+
+        /// <summary>
+        /// Gets a System.DateTimeOffset object that is set to the current date and time
+        /// on the current computer, with the offset set to the local time's offset from
+        /// Coordinated Universal Time (UTC).
+        /// </summary>
+        DateTimeOffset OffsetNow { get; }
+
+        /// <summary>
+        /// Gets a System.DateTimeOffset object whose date and time are set to the current
+        /// Coordinated Universal Time (UTC) date and time and whose offset is System.TimeSpan.Zero.
+        /// </summary>
+        DateTimeOffset OffsetUtcNow { get; }
     }
 }
